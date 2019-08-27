634b0d4dbfbe054e62f621201623223773cea15c
Kun Zhang <kunzhang1110@hotmail.com>
2019-08-27 13:42:07 +1000

    Add webpack.prod.cofig.js

---
+++ App/Talent.App.WebApp/wwwroot/js/react/webpack.prod.config.js
@@ -0,0 +1,52 @@
+﻿var webpack = require('webpack');
+
+var Identity_URL = {
+    production: "https://talentservicesidentity20190826095636.azurewebsites.net",
+    development: "http://localhost:60998",
+}
+
+var environment = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+module.exports = env => {
+
+
+    return {
+        context: __dirname,
+        entry: {
+            homePage: './ReactScripts/Home.js'
+        },
+        output:
+        {
+            path: __dirname + "/dist",
+            filename: "[name].bundle.js"
+        },
+        watch: true,
+        mode: 'production',
+        plugins: [
+            new webpack.DefinePlugin({
+                'Identity_URL': Identity_URL[environment]
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['babel-preset-env', 'babel-preset-react']
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    loaders: [
+                        'style-loader',
+                        'css-loader?modules'
+                    ]
+                }
+            ]
+        }
+    }
+}
